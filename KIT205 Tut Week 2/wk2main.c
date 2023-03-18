e0254ef1b3cc496692ff0f0fdfa661c68b863f24
mave1 <maeveekelly@gmail.com>
2023-03-18 17:57:44 +1100

    Inplemented the merge function. For the sake of testing, it just merges with a hardcoded second list

--- KIT205 Tut Week 2/wk2main.c
+++ KIT205 Tut Week 2/wk2main.c
@@ -55,13 +55,38 @@ void reverse(List* self)
 
 void merge(List* self, List* other) 
 {
+	List merge_list = new_list();
+	ListNodePtr self_current = self->head;
+	ListNodePtr other_current = other->head;
+	
+	while (self_current != NULL && other_current != NULL) {
+		if (self_current->data <= other_current->data) {
+			insert_at_front(&merge_list, self_current->data);
+			self_current = self_current->next;
+		}
+		else {
+			insert_at_front(&merge_list, other_current->data);
+			other_current = other_current->next;
+		}
+	}
 
+	while (self_current != NULL) {
+		insert_at_front(&merge_list, self_current->data);
+		self_current = self_current->next;
+	}
+
+	while (other_current != NULL) {
+		insert_at_front(&merge_list, other_current->data);
+		other_current = other_current->next;
+	}
+
+	reverse(&merge_list);
 }
-
 
 int list()
 {
 	List my_list = new_list();
+	List other_list = new_list();
 
 	int quit = 0;
 	printf("Linked List \n");
@@ -88,10 +113,18 @@ int list()
 		case 4:
 			reverse(&my_list);
 			break;
+		case 5:
+			insert_at_front(&other_list, 12);
+			insert_at_front(&other_list, 9);
+			insert_at_front(&other_list, 8);
+			insert_at_front(&other_list, 2);
+
+			merge(&my_list, &other_list);
 		}
 	}
 
 	destroy_list(&my_list);
+	destroy_list(&other_list);
 	printf("\n");
 }
 
